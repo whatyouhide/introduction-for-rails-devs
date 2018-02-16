a7a549a619f7dfa3156640dd7bad30a7091d4c63
Andrea Leopardi <an.leopardi@gmail.com>
2018-02-16 12:06:18 -0800

    Elixir 1.5 is fine

--- word_count/mix.exs
+++ word_count/mix.exs
@@ -5,7 +5,7 @@ defmodule WordCount.MixProject do
     [
       app: :word_count,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
